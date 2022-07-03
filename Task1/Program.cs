a3e3840d8ea653b8b698e8f38ee1cf971f439d33
Maksim <dumkin_m@rambler.ru>
2022-07-03 20:08:01 +0300

    Added he program finds the sum of all digits of the number

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//The program finds the sum of all digits of the number(Программа находит сумму всех цифр числа)
+Console.Write("Enter the number: ");
+
+int znums=Convert.ToInt32(Console.ReadLine());
+
+int SumdigitalV2(int znums)
+    {
+        int rezult=0;
+        while (znums>0)
+        {   
+            rezult=rezult+znums%10;
+            znums=znums/10;
+        }
+    
+    return rezult;
+    }
+Console.WriteLine("Sum digital: "+SumdigitalV2(znums));
+
+
+
